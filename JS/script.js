cac29a87aa2fe5dc2c5491182f8c0f50c0ff2574
Javier Gonzales <jagonzacas@hotmail.com>
2018-04-23 10:16:01 +0100

    Last fix before preso

--- JS/script.js
+++ JS/script.js
@@ -19,7 +19,7 @@ carousel();
 
 function carousel() {
 	var i;
-	var x = document.getElementsByClassName("mySlides1");
+	var x = document.getElementsByClassName("myPictures");
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";  
 	}
